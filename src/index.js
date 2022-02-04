be42769337a5892140dd0e1a71e121010aa31799
Wolfgang <wolf@okwolf.com>
2022-02-03 19:34:48 -0800

    update build/release process and include deep freeze by source

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-import deepFreeze from "deep-freeze";
+function deepFreeze(o) {
+  Object.freeze(o);
+
+  Object.getOwnPropertyNames(o).forEach(function (prop) {
+    if (
+      o.hasOwnProperty(prop) &&
+      o[prop] !== null &&
+      (typeof o[prop] === "object" || typeof o[prop] === "function") &&
+      !Object.isFrozen(o[prop])
+    ) {
+      deepFreeze(o[prop]);
+    }
+  });
+
+  return o;
+}
 
 export default function freeze(dispatch) {
   return function frozenDispatch(actionOrState, props) {
